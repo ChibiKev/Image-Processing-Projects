69d6b73692d6dd5e6946c887b626570bc259e3bc
Anthony Bravo <abravo3641@gmail.com>
2019-09-30 21:50:49 -0400

    histo stretching

--- Anthony/CS470.skel/hw1/HW_histoStretch.cpp
+++ Anthony/CS470.skel/hw1/HW_histoStretch.cpp
@@ -10,4 +10,30 @@ using namespace IP;
 void
 HW_histoStretch(ImagePtr I1, int t1, int t2, ImagePtr I2)
 {
+    // copy image header (width, height) of input image I1 to output image I2
+    IP_copyImageHeader(I1, I2);
+
+    // init vars for width, height, and total number of pixels
+    int w = I1->width ();
+    int h = I1->height();
+    int total = w * h;
+    
+    // init lookup table
+    int i, lut[MXGRAY];
+    for(i=0; i<MXGRAY; ++i) {
+        double result = ( (double)(i-t1)/(t2-t1) )*255;
+        if(result > 255) result = 255;
+        if(result < 0) result = 0;
+        lut[i] = result;
+    }
+
+    // declarations for image channel pointers and datatype
+    ChannelPtr<uchar> p1, p2;
+    int type;
+
+    // visit all image channels of input and evaluate output image
+    for(int ch=0; IP_getChannel(I1, ch, p1, type); ch++) {    // get input  pointer for channel ch
+        IP_getChannel(I2, ch, p2, type);        // get output pointer for channel ch
+        for(i=0; i<total; i++) *p2++ = lut[*p1++];    // use lut[] to eval output
+    }
 }

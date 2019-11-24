deb23076cd7c8bdd13aed0cacee55899ee003ce6
ChibiKev <k3v.ch3n@gmail.com>
2019-11-24 17:10:43 -0500

    DFT1D Done

--- Kevin/CS470.skel/hw3/dft1D.c
+++ Kevin/CS470.skel/hw3/dft1D.c
@@ -1,49 +1,84 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+
 #define PI 3.1415927
 #define MAG(i)		(sqrt(Fr[i]*Fr[i] + Fi[i]*Fi[i]))
 #define PHASE(i)	(atan2(Fi[i],Fr[i]))
 
-main() {
-	int u, x, N;
-	double c, s, real, imag, cos(), sin();
-	double Fr[8], Fi[8], fr[8], fi[8];
+int main(int argc, char *argv[]) 
+{
+	if (argc == 4){
+		FILE *input, *output; 								// Create File Pointer
+		char *in  = argv[1]; 								// First Argument, Input
+		int dir = atoi(argv[2]); 							// Second Argument, Dir
+		char *out = argv[3]; 								// Third Arugment, Output
+		input  = fopen(in, "r"); 							// Read Input
+		output = fopen(out, "w"); 							// Write Output
 
-	N = 8;
-	for(x=0; x<N; x++) {
-		fr[x] = 2*(x+1);
-		fi[x] = 0;
-	}
+		int u, x, N;
+		double c, s, real, imag, cos(), sin();
 
-	for(u=0; u<N; u++) {		/* compute spectrum over all freq u */
-		real = imag = 0;	/* reset real, imag component of F(u) */
-		for(x=0; x<N; x++) {	/* visit each input pixel */
-			c =  cos(2.*PI*u*x/N);
-			s = -sin(2.*PI*u*x/N);
-			real += (fr[x]*c - fi[x]*s);
-			imag += (fr[x]*s + fi[x]*c);
+		int width, height; 									// Declare Width and Height
+		fscanf(input, "%d\t%d", &width, &height); 			// Get Values of Width and Height
+		N = height; // N = Height
+		float *Fr = malloc(sizeof(float) * N); 				// Store Values
+		float *Fi = malloc(sizeof(float) * N); 				// Store Values
+		float *fr = malloc(sizeof(float) * N); 				// Store Values
+		float *fi = malloc(sizeof(float) * N); 				// Store Values
+		fprintf(output, "%d\t%d\n", width, height); 		// Write Width and Height In First Row
+
+		if (dir == 0){ 										// Foward DFT
+			for (x = 0; x < N; x++){
+				fscanf(input, "%f\t%f", &fr[x], &fi[x]);
+			}
+			for (u = 0; u < N; u++){ 						/* compute spectrum over all freq u */
+				real = imag = 0; 							/* reset real, imag component of F(u) */
+				for (x = 0; x < N; x++){ 					/* visit each input pixel */
+					c =  cos(2.*PI*u*x/N);
+					s = -sin(2.*PI*u*x/N);
+					real += (fr[x]*c - fi[x]*s);
+					imag += (fr[x]*s + fi[x]*c);
+				}
+				Fr[u] = real / N;
+				Fi[u] = imag / N;
+				fprintf(output, "%f\t%f\n", Fr[u], Fi[u]); 	// Write to Output
+			}
 		}
-		Fr[u] = real / N;
-		Fi[u] = imag / N;
-	}
-	for(u=0; u<N; u++)
-		printf("Fr[%d] = %f\t\tFi[%d] = %f\n",u,Fr[u],u,Fi[u]);
-	printf("\n");
-	for(u=0; u<N; u++)
-		printf("|F(%d)| = %f\t\tPhase = %f\n",u,MAG(u), PHASE(u));
-	printf("\n");
-
-	for(x=0; x<N; x++) {		/* compute each output pixel */
-		real = imag = 0;
-		for(u=0; u<N; u++) {
-			c = cos(2.*PI*u*x/N);
-			s = sin(2.*PI*u*x/N);
-			real += (Fr[u]*c - Fi[u]*s);
-			imag += (Fr[u]*s + Fi[u]*c);
+		else if(dir == 1){ // Inverse DFT
+			for (x = 0; x < N; x++){
+				fscanf(input, "%f\t%f", &Fr[x], &Fi[x]);
+			}
+			for (x = 0; x < N; x++){
+				real = imag = 0;
+				for (u = 0; u < N; u++)
+				{
+					c = cos(2.*PI*u*x/N);
+					s = sin(2.*PI*u*x/N);
+					real += (Fr[u]*c - Fi[u]*s);
+					imag += (Fr[u]*s + Fi[u]*c);
+				}
+				fr[x] = real;
+				fi[x] = imag;
+				fprintf(output, "%f\t%f\n", fr[x], fi[x]); 	// Write to Output
+			}
 		}
-		fr[x] = real;
-		fi[x] = imag;
+		else{
+			printf("Invalid Dir\n");
+			printf("Dir = 0, Forward DFT\n");
+			printf("Dir = 1, Inverse DFT\n");
+		}
+		free(Fr); 											// Free Fr
+		free(Fi); 											// Free Fi
+		free(fr); 											// Free fr
+		free(fi); 											// Free fi
+		fclose(input); 										// Close Input File
+		fclose(output); 									// Close Output File
 	}
-	for(x=0; x<N; x++)
-		printf("fr[%d] = %f\t\tfi[%d] = %f\n",x,fr[x],x,fi[x]);
+	else
+	{
+		printf("Invalid Arguments.\n");
+		printf("Format: ./dtft1D Input Dir Output\n");
+	}
+	return 0;
 }
